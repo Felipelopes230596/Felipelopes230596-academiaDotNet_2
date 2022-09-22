386af17c2e66e9fd6592487d3fc977eeb9db2b41
[Felipe Lopes] <[felipe.rev@otmail.com]>
2022-09-22 19:35:23 -0300

    Execrcios ate o 6

---
+++ lista-exercicios-1/lista-exercicios-1/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace lista_exercicios_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercício 1
+
+           double a, b;
+           Console.WriteLine("Escreva o primeiro número: ");
+           a = double.Parse(Console.ReadLine());
+           Console.WriteLine("Escreva o segundo número: ");
+           b = double.Parse(Console.ReadLine());
+           Console.WriteLine("O resultado da média aritimética é " + (a+b)/2);
+
+
+           //Exercício 2
+           double a, b, c, d;
+           Console.WriteLine("Escreva o primeiro número: ");
+           a = double.Parse(Console.ReadLine());
+           Console.WriteLine("Escreva o segundo número: ");
+           b = double.Parse(Console.ReadLine());
+           Console.WriteLine("Escreva o primeiro número: ");
+           c = double.Parse(Console.ReadLine());
+           Console.WriteLine("Escreva o segundo número: ");
+           d = double.Parse(Console.ReadLine());
+           Console.WriteLine("O resultado da média aritimética é " + (a + b + c + d) / 4);
+
+
+
+           //Exercício 3
+
+           int a = 2, b = 3, c;
+           Console.WriteLine("Os valores de A="+a+" B="+b);
+           c = a;
+           a = b;
+           b = c;
+           Console.WriteLine("Os novos valores de A=" + a + " B=" + b);
+
+
+           //Exercício 4
+
+           string d, m;
+           int a;
+           Console.WriteLine("Digite a data desejada, DDMMAAAA: ");
+           d = Console.ReadLine();
+           m = Console.ReadLine();
+           a = int.Parse(Console.ReadLine());
+           Console.WriteLine("A data no formato AAAAMMDD é: " + a+"/"+m+"/"+d+ " e no formato AAMMDD é: "+ a.ToString().Substring(2, 2)+"/"+m+"/"+d);
+
+           //Exercício 5
+
+           int km, l;
+           Console.WriteLine("Digite a distância percorida: ");
+           km = int.Parse(Console.ReadLine());
+           Console.WriteLine("Digite a quantidade de litros consumiudo: ");
+           l = int.Parse(Console.ReadLine());
+           Console.WriteLine("O consumo médio foi de "+km/l+"km/l");
+
+           
+            //Exercício 6
+
+            double codA, codB, qtdA, qtdB, vlrA, vlrB, ipi;
+
+            Console.WriteLine("Digite o código do produto A: ");
+            codA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade do produto A: ");
+            qtdA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do produto A: ");
+            vlrA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o código do produto B: ");
+            codB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade do produto A: ");
+            qtdB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do produto A: ");
+            vlrB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o percentual de IPI a ser aplicado: ");
+            ipi = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor com IPI do produto A é: " + (ipi / vlrA) * 100);
+            Console.WriteLine("O valor com IPI do produto B é: " + (ipi / vlrB) * 100);
+            */
+        }
+    }
+}
